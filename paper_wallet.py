9dccd221d5078e5d39bd319f3917b70daefa2fc5
adhigame989 <adhigame989@gmail.com>
2026-05-31 18:49:09 +0700

    Update paper_wallet.py

--- paper_wallet.py
+++ paper_wallet.py
@@ -2,39 +2,25 @@ import json
 
 WALLET_FILE = "paper_wallet.json"
 
-
 def get_balance():
-
     try:
-        with open(
-            WALLET_FILE,
-            "r"
-        ) as f:
-
+        with open(WALLET_FILE, "r") as f:
             data = json.load(f)
-
-            return data.get(
-                "balance",
-                0
-            )
-
+            return data.get("balance", 0)
     except:
-
         return 0
 
+def save_balance(balance):
+    with open(WALLET_FILE, "w") as f:
+        json.dump({"balance": balance}, f)
 
-def save_balance(
-    balance
-):
+def add_balance(amount):
+    balance = get_balance()
+    save_balance(balance + amount)
 
-    with open(
-        WALLET_FILE,
-        "w"
-    ) as f:
+def reduce_balance(amount):
+    balance = get_balance()
+    save_balance(balance - amount)
 
-        json.dump(
-            {
-                "balance": balance
-            },
-            f
-        )
+def reset_balance(amount=1000000):
+    save_balance(amount)
